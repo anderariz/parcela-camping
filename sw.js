f2eb8361d080d6014aef1ec686eadc3715e255dd
anderariz <anderariz@gmail.com>
2026-08-19 10:01:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
-const CACHE='camping-layout-v7-iphone';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+const CACHE='camping-layout-v8-iphone';
+const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.mode==='navigate' || req.destination==='document'){
+    event.respondWith(
+      fetch(req,{cache:'no-store'})
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put('./index.html',copy));
+          return res;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(req,{cache:'no-store'})
+      .then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy));
+        return res;
+      })
+      .catch(()=>caches.match(req))
+  );
+});
