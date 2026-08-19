c41898cbc6d38fbb8191bb01fc78a1fab2e70f0e
anderariz <anderariz@gmail.com>
2026-08-19 11:16:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='camping-layout-v8-iphone';
+const CACHE='camping-layout-v9-iphone';
 const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
