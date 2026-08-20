a1fc9800e25698880e18ccda3f4d4fe6de0cdabe
anderariz <anderariz@gmail.com>
2026-08-20 15:12:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='camping-v13-1-real-assets';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));});
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister().then(()=>self.clients.claim())));
